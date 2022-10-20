b84b6398d84049ae173fd06e5efd1a892532c7fa
MyLiege23 <hsabel@outlook.com>
2022-10-20 13:34:03 -0600

    Add panel for JS exercises and testing
    Followed instructions for DOM manipulation from the Odin Project.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,47 @@
-const container = document.querySelector('#container');
+const grayPanel = document.querySelector("#jsPanel");
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const redParagraph = document.createElement("p");
+redParagraph.style.color = "red";
+redParagraph.classList.add("quote-text");
+redParagraph.textContent = "Hey, I'm red!";
 
-container.appendChild(content);
+const blueH3 = document.createElement("h3");
+blueH3.classList.add("title");
+blueH3.setAttribute("style", "color: blue;");
+blueH3.textContent = "I'm a blue h3!";
+
+const pinkContainer = document.createElement("div");
+pinkContainer.classList.add("container");
+pinkContainer.setAttribute(
+  "style",
+  "background: pink; border-radius: 5px; border: solid black;"
+);
+
+const pinkContainerH1 = document.createElement("h1");
+pinkContainerH1.classList.add("title");
+pinkContainerH1.textContent = "I'm a div!";
+
+const pinkContainerParagraph = document.createElement("p");
+pinkContainerParagraph.classList.add("title-caption");
+pinkContainerParagraph.textContent = "ME TOO!";
+
+
+
+//pinkContainer.children.setAttribute("style", "color: black;")
+
+grayPanel.appendChild(redParagraph);
+grayPanel.appendChild(blueH3);
+pinkContainer.appendChild(pinkContainerH1);
+pinkContainer.appendChild(pinkContainerParagraph);
+grayPanel.appendChild(pinkContainer);
+
+/*
+
+  1  a <p> with red text that says “Hey I’m red!”
+  2  an <h3> with blue text that says “I’m a blue h3!”
+  3  a <div> with a black border and pink background color with the following elements inside of it:
+    1    another <h1> that says “I’m in a div”
+    2    a <p> that says “ME TOO!”
+    3    Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+
+*/
